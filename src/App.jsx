f11ae163aa50caf9e928d7c1399aab2a166cb07f
StringLOVERgirl <marchenkostar@gmail.com>
2025-11-02 17:59:15 +0300

    active and copy

--- src/App.jsx
+++ src/App.jsx
@@ -74,6 +74,7 @@ class Cont extends React.Component{
     <div className="mockupcont">
       <div className="mockuptext">mockup</div>
       <div className="mockup"></div>
+      <div className="copy copymob">2025© Meg. All rights reserved.</div>
     </div>
     <div className="copy">2025© Meg. All rights reserved.</div>
 
@@ -81,43 +82,10 @@ class Cont extends React.Component{
 }
 
 function App() {
-  // конструткор нужен чтобы натсрйоить начльное состояние и свойства комопнента
-  // перед рендерингом; выполняется ОДИН раз перед первым рендером
-  // в нем нужно привязать контекст (bind) к методам
-  // this в классовых комп - ссылка на экз класса
-  // это объкт - в нем хранятся свойства стейт и метода
-  // В классах методы — это обычные функции, не "прикреплённые" жёстко к объекту.
-  // Когда метод передаётся как callback (например, в onClick), 
-  // JavaScript вызывает его в другом контексте
-  // в конструторе писать мтоды this.increment = this.increment.bind(this); 
-  // может быть только один стейт
-  // (можно не писать если метод стрелочный)
-  // ...
-  // increment() {
-    // this.setState({ count: this.state.count + 1 }); // Теперь работает
-  // }
-
-  // жизненный цикл делится на 3 этапа 
-  // монториваное - изменение - размонтирование
-
-  // монитрование - конструтор - рнедер - componentDidMount()
-  // componentDidMount() - после первогоодин раз рендера аналог юс эффект
-  // пишится как обыная функция вызыается реатком сам
-
-  // обновление
-  // shouldComponentUpdate - аналог юс эффект с массивом зависимсотей 
-  // реакт вызывает перед рендером только 
-  // реашет нужен ли рендер при изменении данных
-  // регуляция рендера через возрващаемое значени фнукцие - тру или фолс
-  // возварт вручную через ретерн 
-  // т е shouldComponentUpdate работвт как мемо но с дпо логикой для контроля?
-
-  // размонтирование - componentWillUnmount - очистка подписок
-  // componentWillUnmount(): Вызывается перед удалением.
-
   return (
     <div className="App">
       <Cont></Cont>
+      {/* <div className="copy copymob">2025© Meg. All rights reserved.</div> */}
     </div>
   )
 }
